aaeb5e878e41cdeeb799ff95591ba2addb3bac8a
Curtis Babnik <curtisbabnik@gmail.com>
2022-04-17 17:34:20 -0700

    Mostly set the css layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import logo from './logo.svg';
+import '@aws-amplify/ui-react/styles.css'
 import './App.css';
 
-import '@aws-amplify/ui-react/styles.css'
-import { Header } from "./ui-components";
-import { Sidebar } from "./ui-components";
+// TODO:
+// fix whitespace at top
+// move styling into CSS
+// select some better fonts (lesson)
+// replace text with Figma components
+// resize width of filters to largest filter
+// resize heights dynamically
 
 function App() {
    return (
-      <div>
-         <header>
-            <Header />
+      <div className='app'>
+         <header className='header'>
+            <h1>IndiasJobBoard</h1>
          </header>
-         <div id="sidebar">
-            <Sidebar />
+         <div className='sidebar'>
+            <h2>Filters</h2>
+         </div>
+         <div className='main'>
+            <div className='jobsHeader'>
+               <h2>Jobs</h2>
+            </div>
+            <div className='jobsColumn'>
+               <h4>Todo:</h4>
+            </div>
+            <div className='jobsColumn'>
+               <h4>In Progress:</h4>
+            </div>
+            <div className='jobsColumn'>
+               <h4>Done:</h4>
+            </div>
          </div>
       </div>
    );
